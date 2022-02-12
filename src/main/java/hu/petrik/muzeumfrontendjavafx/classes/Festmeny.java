15416bc1798efe95a4936b7b92e6200b75fd84b4
pivarcsi.gergely <gergely.pivarcsi@gmail.com>
2022-02-12 16:57:05 +0100

    Painting class modified.

--- src/main/java/hu/petrik/muzeumfrontendjavafx/classes/Festmeny.java
+++ src/main/java/hu/petrik/muzeumfrontendjavafx/classes/Festmeny.java
@@ -5,4 +5,43 @@ public class Festmeny {
     private String title;
     private int year;
     private boolean on_display;
+
+    public Festmeny(int id, String title, int year, boolean on_display) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.on_display = on_display;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isOn_display() {
+        return on_display;
+    }
+
+    public void setOn_display(boolean on_display) {
+        this.on_display = on_display;
+    }
 }
