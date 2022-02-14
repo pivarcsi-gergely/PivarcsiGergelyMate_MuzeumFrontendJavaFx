2e050239b133f266e7e4d8da236e4b49bbb2a537
pivarcsi.gergely <gergely.pivarcsi@gmail.com>
2022-02-14 19:19:28 +0100

    Listázás kész, következő a hozzáadás

---
+++ src/main/java/hu/petrik/muzeumfrontendjavafx/controllers/MuzeumController.java
@@ -0,0 +1,109 @@
+package hu.petrik.muzeumfrontendjavafx.controllers;
+
+import hu.petrik.muzeumfrontendjavafx.Controller;
+import hu.petrik.muzeumfrontendjavafx.api.MuzeumApi;
+import hu.petrik.muzeumfrontendjavafx.classes.Festmeny;
+import hu.petrik.muzeumfrontendjavafx.classes.Szobor;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MuzeumController extends Controller {
+
+    @FXML private Button szoborEditBtn;
+    @FXML private Button szoborUjBtn;
+    @FXML private Button szoborTorolBtn;
+    @FXML private TableView<Szobor> szoborTable;
+    @FXML private TableColumn<Szobor, String> colSzoborTulaj;
+    @FXML private TableColumn<Szobor, Integer> colSzoborMag;
+    @FXML private TableColumn<Szobor, Integer> colSzoborAr;
+    @FXML private Button festmenyEditBtn;
+    @FXML private Button festmenyUjBtn;
+    @FXML private Button festmenyTorolBtn;
+    @FXML private TableView<Festmeny> festmenyTable;
+    @FXML private TableColumn<Festmeny, String> colFestmenyCim;
+    @FXML private TableColumn<Festmeny, Integer> colfestmenyEv;
+    @FXML private TableColumn<Festmeny, String> colFestmenyKiallitott;
+
+    public void initialize() {
+        colSzoborTulaj.setCellValueFactory(new PropertyValueFactory<Szobor, String>("person"));
+        colSzoborMag.setCellValueFactory(new PropertyValueFactory<>("height"));
+        colSzoborAr.setCellValueFactory(new PropertyValueFactory<>("price"));
+        szoborFeltolt();
+
+        colFestmenyCim.setCellValueFactory(new PropertyValueFactory<>("title"));
+        colfestmenyEv.setCellValueFactory(new PropertyValueFactory<>("year"));
+        colFestmenyKiallitott.setCellValueFactory(new PropertyValueFactory<>("title"));
+        festmenyFeltolt();
+    }
+
+    @FXML
+    public void onEditSzoborClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onUjSzoborClicked(ActionEvent actionEvent) {
+        try {
+            Controller hozzaadas = ujAblak("hozzaad-szobor-view.fxml", "Szobor hozzáadása", 320, 400);
+            hozzaadas.getStage().setOnCloseRequest(event -> szoborFeltolt());
+            hozzaadas.getStage().show();
+        } catch (Exception e) {
+            hibaKiir(e);
+        }
+    }
+
+    @FXML
+    public void onTorolSzoborClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onFestmenyTorolClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onUjFestmenyClicked(ActionEvent actionEvent) {
+        try {
+            Controller hozzaadas = ujAblak("hozzaad-festmeny-view.fxml", "Festmény hozzáadása", 320, 400);
+            hozzaadas.getStage().setOnCloseRequest(event -> festmenyFeltolt());
+            hozzaadas.getStage().show();
+        } catch (Exception e) {
+            hibaKiir(e);
+        }
+    }
+
+    @FXML
+    public void onEditFestmenyClicked(ActionEvent actionEvent) {
+    }
+
+    private void festmenyFeltolt() {
+        try {
+            List<Festmeny> festmenyLista = MuzeumApi.getFestmenyek();
+            festmenyTable.getItems().clear();
+            for (Festmeny festmeny: festmenyLista) {
+                festmenyTable.getItems().add(festmeny);
+            }
+        }
+        catch (IOException e){
+            hibaKiir(e);
+        }
+    }
+
+    private void szoborFeltolt() {
+        try {
+            List<Szobor> szoborLista = MuzeumApi.getSzobrok();
+            szoborTable.getItems().clear();
+            for (Szobor szobor: szoborLista) {
+                szoborTable.getItems().add(szobor);
+            }
+        }
+        catch (IOException e){
+            hibaKiir(e);
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,11 @@ module hu.petrik.muzeumfrontendjavafx {
     requires com.google.gson;
 
     opens hu.petrik.muzeumfrontendjavafx to javafx.fxml, com.google.gson;
+    opens hu.petrik.muzeumfrontendjavafx.api to com.google.gson;
+    opens hu.petrik.muzeumfrontendjavafx.classes to com.google.gson;
+    exports hu.petrik.muzeumfrontendjavafx.classes;
+    exports hu.petrik.muzeumfrontendjavafx.api;
     exports hu.petrik.muzeumfrontendjavafx;
+    exports hu.petrik.muzeumfrontendjavafx.controllers;
+    opens hu.petrik.muzeumfrontendjavafx.controllers to com.google.gson, javafx.fxml;
 }
